58bdc6a2412946ffb04be3fae72f00cf48ed570a
danocide <dan.c.reilly@gmail.com>
2023-02-01 19:36:21 -0500

    added delete function, cleaned up and formatted

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -4,48 +4,68 @@ const fs = require('fs')
 const { v4: uuidv4 } = require('uuid');
 
 router.get('/notes', (req, res) => {
-  fs.readFile('db/db.json','utf-8', (err,data) => {
-    return err ? console.log(err) : res.json(JSON.parse(data))})
+    fs.readFile('db/db.json', 'utf-8', (err, data) => {
+        return err ? console.log(err) : res.json(JSON.parse(data))
+    })
 });
 
 router.post('/notes', (req, res) => {
-  console.log(req.body);
+    console.log(req.body);
 
-  const { title, text } = req.body
+    const { title, text } = req.body
 
-  if (title && text) {
-    const newNote = {
-      title,
-      text,
-      id: uuidv4()
-    };
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            id: uuidv4()
+        };
 
-    res.json(newNote)
+        res.json(newNote)
 
-    fs.readFile('db/db.json','utf-8', (err,data) => {
-      if (err) {
-        console.error(err);
-      } else {
-        
-        const parsedReviews = JSON.parse(data);
+        fs.readFile('db/db.json', 'utf-8', (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
 
-        parsedReviews.push(newNote);
+                const parsedReviews = JSON.parse(data);
 
-        fs.writeFile(
-          'db/db.json',
-          JSON.stringify(parsedReviews, null, 4),
-          (writeErr) =>
-            writeErr
-              ? console.error(writeErr)
-              : console.info('Successfully updated Notes!')
-              
-        );
-      }
-    });
-    
-  } 
+                parsedReviews.push(newNote);
+
+                fs.writeFile(
+                    'db/db.json',
+                    JSON.stringify(parsedReviews, null, 4),
+                    (writeErr) =>
+                        writeErr
+                            ? console.error(writeErr) : console.info('Note Added')
+                );
+            }
+        });
+    }
 });
 
+router.delete('/notes/:id', (req, res) => {
+    let deleteid = req.params.id;
+    fs.readFile('db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.log(err);
+        }
+        let noteData = JSON.parse(data);
+        for (let i = 0; i < noteData.length; i++) {
+            if (deleteid == noteData[i].id) {
+                noteData.splice(i, 1);
+                fs.writeFile('db/db.json', JSON.stringify(noteData, null, 4), (err) => {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log('Note Deleted');
+                    }
+                });
+            };
+        };
+    });
 
+    res.end();
+});
 
 module.exports = router
